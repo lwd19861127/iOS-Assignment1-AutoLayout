71ce5d3db71af55acce7d18bbb40871c9d7474af
WendaLi <lwd19861127@hotmail.com>
2020-04-18 13:41:34 +0800

    update

--- AutoLayoutStarter/ViewController.swift
+++ AutoLayoutStarter/ViewController.swift
@@ -51,11 +51,52 @@ class ViewController: UIViewController {
   //Wenda Li
     
     let purpleBox: UIView = {
-        let purple = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
-        purple.frame = CGRect.zero
+        let purple = UIView()
         purple.translatesAutoresizingMaskIntoConstraints = false
         purple.backgroundColor = .purple
         return purple
+    }()
+    
+    let redBox: UIView = {
+        let red = UIView()
+        red.translatesAutoresizingMaskIntoConstraints = false
+        red.backgroundColor = .red
+        return red
+    }()
+    
+    let orangeBox1: UIView = {
+        let orange = UIView()
+        orange.translatesAutoresizingMaskIntoConstraints = false
+        orange.backgroundColor = .orange
+        return orange
+    }()
+    
+    let orangeBox2: UIView = {
+        let orange = UIView()
+        orange.translatesAutoresizingMaskIntoConstraints = false
+        orange.backgroundColor = .orange
+        return orange
+    }()
+    
+    let blueBox2: UIView = {
+        let blue = UIView()
+        blue.translatesAutoresizingMaskIntoConstraints = false
+        blue.backgroundColor = .blue
+        return blue
+    }()
+    
+    let blueBox3: UIView = {
+        let blue = UIView()
+        blue.translatesAutoresizingMaskIntoConstraints = false
+        blue.backgroundColor = .blue
+        return blue
+    }()
+    
+    let blueBox4: UIView = {
+        let blue = UIView()
+        blue.translatesAutoresizingMaskIntoConstraints = false
+        blue.backgroundColor = .blue
+        return blue
     }()
     
   //end
@@ -77,9 +118,59 @@ class ViewController: UIViewController {
     
     //Wenda Li
     mainView.addSubview(purpleBox)
-    NSLayoutConstraint(item: purpleBox, attribute: .leading, relatedBy: .equal, toItem: mainView, attribute: .leadingMargin, multiplier: 1.0, constant: 0.0).isActive = true
-    NSLayoutConstraint(item: purpleBox, attribute: .trailing, relatedBy: .equal, toItem: mainView, attribute: .trailingMargin, multiplier: 1.0, constant: 0.0).isActive = true
-    NSLayoutConstraint(item: purpleBox, attribute: .width, relatedBy: .equal, toItem: mainView, attribute:.width, multiplier: 0.7, constant:0.0).isActive = true
+    let purpleHeight: CGFloat = 80
+    purpleBox.heightAnchor.constraint(equalToConstant: purpleHeight).isActive = true
+    purpleBox.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: 300).isActive = true
+    purpleBox.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -30).isActive = true
+    purpleBox.bottomAnchor.constraint(equalTo: mainView.bottomAnchor, constant: -30).isActive = true
+   
+    mainView.addSubview(redBox)
+    let redWidth: CGFloat = 200
+    let redHeight: CGFloat = 70
+    redBox.widthAnchor.constraint(equalToConstant: redWidth).isActive = true
+    redBox.heightAnchor.constraint(equalToConstant: redHeight).isActive = true
+    redBox.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 30).isActive = true
+    redBox.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -30).isActive = true
+    
+    redBox.addSubview(orangeBox1)
+    let orange1Width: CGFloat = 102
+    let orange1Height: CGFloat = 50
+    orangeBox1.widthAnchor.constraint(equalToConstant: orange1Width).isActive = true
+    orangeBox1.heightAnchor.constraint(equalToConstant: orange1Height).isActive = true
+    orangeBox1.centerYAnchor.constraint(equalTo: redBox.centerYAnchor).isActive = true
+    orangeBox1.leadingAnchor.constraint(equalTo: redBox.leadingAnchor, constant: 10).isActive = true
+    
+    redBox.addSubview(orangeBox2)
+    let orange2Width: CGFloat = 68
+    let orange2Height: CGFloat = 50
+    orangeBox2.widthAnchor.constraint(equalToConstant: orange2Width).isActive = true
+    orangeBox2.heightAnchor.constraint(equalToConstant: orange2Height).isActive = true
+    orangeBox2.centerYAnchor.constraint(equalTo: redBox.centerYAnchor).isActive = true
+    orangeBox2.trailingAnchor.constraint(equalTo: redBox.trailingAnchor, constant: -10).isActive = true
+
+    
+    let blueStackView = UIStackView(arrangedSubviews: [
+    blueBox2, blueBox3, blueBox4])
+    blueStackView.translatesAutoresizingMaskIntoConstraints = false
+    blueStackView.axis = .vertical
+    blueStackView.alignment = .center
+    blueStackView.distribution = .fillEqually
+    
+    let blueHeight: CGFloat = 50
+    blueBox2.widthAnchor.constraint(equalTo: blueStackView.widthAnchor).isActive = true
+    blueBox2.heightAnchor.constraint(equalToConstant: blueHeight).isActive = true
+    blueBox3.widthAnchor.constraint(equalTo: blueStackView.widthAnchor).isActive = true
+    blueBox3.heightAnchor.constraint(equalToConstant: blueHeight).isActive = true
+    blueBox4.widthAnchor.constraint(equalTo: blueStackView.widthAnchor).isActive = true
+    blueBox4.heightAnchor.constraint(equalToConstant: blueHeight).isActive = true
+
+    mainView.addSubview(blueStackView)
+    NSLayoutConstraint.activate([
+    blueStackView.centerXAnchor.constraint(equalTo: mainView.centerXAnchor),
+    blueStackView.widthAnchor.constraint(equalToConstant: 50),
+    blueStackView.heightAnchor.constraint(equalTo: mainView.heightAnchor),
+    ])
+    
     //end
     
     let buttStackView = UIStackView(arrangedSubviews: [
